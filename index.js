84bbf9e3f34e040840cea4bdf3f68e2838a24f73
didipta <66532706+didipta@users.noreply.github.com>
2023-02-21 20:13:06 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socket= require("socket.io");
+const port=process.env.PORT || 2000;
+const io=socket(server,{
+    cors:{
+        origin:"*",
+        
+    }
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("Thesis web site")
+});
+server.listen(port, () =>
+{
+    console.log(`Listening to port ${port}`);
+    global.onlineUsers=new Map();
+
+io.on("connection",(socket)=>{
+    global.chatsocket=socket;
+    socket.on("addUser",(id)=>{
+        onlineUsers.set(id,socket.id);
+    })
+    socket.on("send-msg",(date)=>{
+        const sendUserSocket=onlineUsers.get(date.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("msg-receive",date.message);
+        }
+    })
+})
+})
